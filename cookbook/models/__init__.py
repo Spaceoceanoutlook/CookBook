355c212f7add2dd03510b3011d496240124aafb6
Spaceoceanoutlook <spaceocean@outlook.com>
2025-11-19 15:04:35 +0500

    created tables

--- cookbook/models/__init__.py
+++ cookbook/models/__init__.py
@@ -1,8 +1,7 @@
-from cookbook.models.recipe_ingredient import RecipeIngredient
-
 from cookbook.models.base import Base
 from cookbook.models.ingredient import Ingredient
 from cookbook.models.recipe import Recipe
+from cookbook.models.recipe_ingredients import RecipeIngredient
 
 __all__ = [
     "Base",
